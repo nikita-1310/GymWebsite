6b1cf9ff5fcfb44b4eaff7a4bb69eb1c9320a448
nikita <nikitaagarwal8755@gmail.com>
2023-04-10 15:01:09 +0530

    Updated code

---
+++ fit-club/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hero from './Components/Hero/Hero';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Hero />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ fit-club/src/Components/Hero/Hero.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './Hero.css';
+import hero_image from '../../assets/hero_image.png';
+import Heart from '../../assets/heart.png';
+import hero_image_back from '../../assets/hero_image_back.png';
+import Calories from '../../assets/calories.png';
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="left-h">
+          <Header />
+
+          <div className="the-best-ad">
+            <div></div>
+            <span>the best fitness club in the town</span>
+          </div>
+
+        <div className="hero-text">
+          <div>
+            <span className='stroke-text'>Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>
+              Ideal body
+            </span>
+          </div>
+          <div>
+            <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+          </div>
+        </div>
+
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        <div className="hero-button">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+
+        </div>
+
+{/* ------------------- Right side */}
+        <div className="right-h">
+          <button className="btn">Join Now</button>
+
+          <div className="heart-rate">
+            <img src={Heart} alt="" />
+            <span>Heart Rate</span><span> 116 bpm</span>
+          </div>
+
+          <img src={hero_image} alt="" className='hero-image' />
+          <img src={hero_image_back} alt="" className='hero-image-back' />
+
+          <div className="calories">
+            <img src={Calories} alt="" />
+            <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+            </div>
+            
+          </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
