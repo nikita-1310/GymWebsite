16692b28bbcd59e3db44403e8bf80a5cd618405a
Nikita Agarwal <115351051+nikita-1310@users.noreply.github.com>
2023-04-13 18:25:49 +0530

    Update Footer.jsx

--- fit-club/src/Components/Footer/Footer.jsx
+++ fit-club/src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 import Github from '../../assets/github.png'
 import Instagram from '../../assets/instagram.png'
 import Linkedin from '../../assets/linkedin.png'
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/logo.png';
 const Footer = () => {
   return (
     <div className="footer-container">
